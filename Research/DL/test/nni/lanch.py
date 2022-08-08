097cbb19b7bfbab6dcabfedc7f47b3916bc42de9
sgf <1085534723@qq.com>
2022-08-08 20:00:44 +0800

    nni

---
+++ Research/DL/test/nni/lanch.py
@@ -0,0 +1,13 @@
+import nni
+from nni.experiment import Experiment
+
+
+# nni.
+# experiment = Experiment('local')
+# nni.
+# experiment.get_experiment_metadata()
+
+nni.get_experiment_id()
+nni.get_trial_id()
+
+# nni.get
